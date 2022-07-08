604e22bc5ad39ac4697e1f76e16c0de2aa8b021a
Balukh Konstantin <baluh.kostya@gmail.com>
2022-07-08 20:20:19 +0300

    IDataSource declaration

---
+++ src/Application/Buzzword.Application.Domain/DataContext/IDataSource.cs
@@ -0,0 +1,19 @@
+﻿using Buzzword.Application.Domain.Entities;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace Buzzword.Application.Domain.DataContext
+{
+    public interface IDataSource : IDisposable
+    {
+        DbSet<User> Users { get; set; }
+        DbSet<UserWord> UserWords { get; set; }
+        DbSet<Group> Groups { get; set; }
+        DbSet<GroupWord> GroupWords { get; set; }
+
+        EntityEntry<TEntity> Entry<TEntity>(TEntity entity) where TEntity : class;
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
