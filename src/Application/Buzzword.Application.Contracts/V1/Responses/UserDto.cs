1367dba9b5524bfa54e41f415a69c0b944082995
Balukh Konstantin <baluh.kostya@gmail.com>
2022-07-09 22:26:51 +0300

    userdto

---
+++ src/Application/Buzzword.Application.Contracts/V1/Responses/UserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Buzzword.Application.Contracts.V1.Responses
+{
+    public class UserDto
+    {
+        public Guid Id { get; set; } = default;
+
+        public string Name { get; set; } = "";
+    }
+}
---
+++ src/Application/Buzzword.Application.Interfaces/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using Buzzword.Application.Contracts.V1.Responses;
+
+namespace Buzzword.Application.Interfaces
+{
+    public interface IUserService
+    {
+        Task<IList<UserDto>> GetUsersAsync();
+        Task<UserDto> GetUserAsync(Guid userId);
+        Task<UserDto> CreateUserAsync(UserDto user);
+        Task<UserDto> UpdateUserAsync(UserDto user);
+        Task<UserDto> DeleteUserASync(Guid userId);
+    }
+}
