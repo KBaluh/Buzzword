20a2d4b3fc559c3f46554e2db418d8903cfb3285
Balukh Konstantin <baluh.kostya@gmail.com>
2022-07-10 01:37:24 +0300

    code cleanup

--- src/UI/Clients/Buzzword.DesktopApp/MauiProgram.cs
+++ src/UI/Clients/Buzzword.DesktopApp/MauiProgram.cs
@@ -1,37 +1,36 @@
-﻿using Microsoft.AspNetCore.Components.WebView.Maui;
-using Buzzword.Pages.Data;
-using Buzzword.Components;
-using Buzzword.Application.Interfaces;
+﻿using Buzzword.Application.Interfaces;
 using Buzzword.Application.WebDomainServices;
-using Buzzword.HttpPolly;
+using Buzzword.Components;
 using Buzzword.DesktopApp.Services;
+using Buzzword.HttpPolly;
+using Buzzword.Pages.Data;
 
 namespace Buzzword.DesktopApp;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-			});
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            });
 
-		builder.Services.AddMauiBlazorWebView();
+        builder.Services.AddMauiBlazorWebView();
 #if DEBUG
-		builder.Services.AddBlazorWebViewDeveloperTools();
+        builder.Services.AddBlazorWebViewDeveloperTools();
 #endif
 
         builder.Services.AddScoped<ThemeInterop>();
         builder.Services.AddSingleton<WeatherForecastService>();
-		builder.Services.AddHttpClient();
+        builder.Services.AddHttpClient();
         builder.Services.AddSingleton<IHttpPollyConnection, HttpPollyConnection>();
         builder.Services.AddSingleton<HttpPollyClient>();
         builder.Services.AddSingleton<IUserService, UserService>();
 
-		return builder.Build();
-	}
+        return builder.Build();
+    }
 }
