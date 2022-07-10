cad4faa35392fcadf51eb5e01156c8005203e98d
Balukh Konstantin <baluh.kostya@gmail.com>
2022-07-10 12:38:31 +0300

    UserWord translate

--- src/Application/Buzzword.Application.Domain/Entities/UserWord.cs
+++ src/Application/Buzzword.Application.Domain/Entities/UserWord.cs
@@ -4,6 +4,8 @@
     {
         public string Word { get; set; } = "";
 
+        public string Translate { get; set; } = "";
+
         public Guid UserId { get; set; }
 
         public User? User { get; set; }
---
+++ src/Application/Buzzword.Application.Domain/Migrations/20220710091604_AddUserWordTranslate.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Buzzword.Application.Domain.Migrations
+{
+    public partial class AddUserWordTranslate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Translate",
+                table: "UserWord",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Translate",
+                table: "UserWord");
+        }
+    }
+}
